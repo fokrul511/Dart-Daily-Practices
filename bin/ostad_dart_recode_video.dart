98d91916e2fe0ca2a1df33755411194a3b733886
fokrul511 <islamfokrul2002@gmail.com>
2023-10-24 08:38:15 +0600

    dart new practics Projects

---
+++ bin/ostad_dart_recode_video.dart
@@ -0,0 +1,5 @@
+import 'package:ostad_dart_recode_video/ostad_dart_recode_video.dart' as ostad_dart_recode_video;
+
+void main(List<String> arguments) {
+  print('Hello world: ${ostad_dart_recode_video.calculate()}!');
+}
---
+++ test/ostad_dart_recode_video_test.dart
@@ -0,0 +1,8 @@
+import 'package:ostad_dart_recode_video/ostad_dart_recode_video.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
