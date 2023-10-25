0fd9103b56c4c567160ed463d9495164f0fa3b70
fokrul511 <islamfokrul2002@gmail.com>
2023-10-25 19:06:03 +0600

    dart list

---
+++ lib/dart_list.dart
@@ -0,0 +1,19 @@
+void main() {
+  var list2 = [
+    0,
+    'Fokrul',
+    333,
+    'sadiya',
+  ]; // this problem solve '?' singh ?list....
+
+  print(list2[0]);
+  print(list2[1]);
+  print(list2[2]);
+  print(list2[3]);
+}
+
+class AddList {
+  addNumber() {
+    return 20 + 2;
+  }
+}
