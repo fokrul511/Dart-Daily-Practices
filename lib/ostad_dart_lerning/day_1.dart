7c8eef11d88e4b23e679954e89d3fdf008e50751
fokrul511 <islamfokrul2002@gmail.com>
2023-10-30 21:51:56 +0600

    dart class 1 , data tyeps

---
+++ lib/ostad_dart_lerning/day_1.dart
@@ -0,0 +1,23 @@
+void main(){
+BasicDart.datatype();
+}
+//datatype....
+class BasicDart{
+static datatype(){
+  int roll;
+  roll = 3444;
+  print('this is your roll: $roll');// int data tyeps.........
+  double cgp;
+  cgp = 3.31;
+  print('your cgp is: $cgp');// dubble data tyeps.........
+  String name;
+  name = 'Fokrul Islam';
+  print('Your name is: $name');// String data tyeps.........
+  bool checkQualification;
+  checkQualification = true;
+  print('your propessoon is Flutter developer: $checkQualification');// bool data tyeps.........
+
+
+}
+
+}
