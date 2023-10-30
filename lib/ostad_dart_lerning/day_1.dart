af53db24eefeb79d78026be0d9f615061de70771
fokrul511 <islamfokrul2002@gmail.com>
2023-10-30 21:59:46 +0600

    update

--- lib/ostad_dart_lerning/day_1.dart
+++ lib/ostad_dart_lerning/day_1.dart
@@ -1,23 +1,45 @@
-void main(){
-BasicDart.datatype();
+void main() {
+  BasicDart.datatype();
+  BasicDart.operator();
 }
+
 //datatype....
-class BasicDart{
-static datatype(){
-  int roll;
-  roll = 3444;
-  print('this is your roll: $roll');// int data tyeps.........
-  double cgp;
-  cgp = 3.31;
-  print('your cgp is: $cgp');// dubble data tyeps.........
-  String name;
-  name = 'Fokrul Islam';
-  print('Your name is: $name');// String data tyeps.........
-  bool checkQualification;
-  checkQualification = true;
-  print('your propessoon is Flutter developer: $checkQualification');// bool data tyeps.........
+class BasicDart {
+  static datatype() {
+    print('/datatype....');
+    int roll;
+    roll = 3444;
+    print('this is your roll: $roll'); // int data tyeps.........
+    double cgp;
+    cgp = 3.31;
+    print('your cgp is: $cgp'); // dubble data tyeps.........
+    String name;
+    name = 'Fokrul Islam';
+    print('Your name is: $name'); // String data tyeps.........
+    bool checkQualification;
+    checkQualification = true;
+    print(
+        'your propessoon is Flutter developer: $checkQualification'); // bool data tyeps.........
+    print('#######################################');
+  }
 
+  static operator() {
+    print('operators.............');
+    // +
+    // -
+    // '/'
+    // %
+    // *
+    // ~/
 
-}
+    int a = 50;
+    int b = 100;
 
+    // add operator
+    int add = a + b;
+    print('add :$add');
+
+    int sub = a - b;
+    print('sub :$sub');
+  }
 }
