2a42666278ad7632397730895684a539653e384d
fokrul511 <islamfokrul2002@gmail.com>
2023-11-05 22:39:09 +0600

    update

---
+++ lib/ostad_dart_lerning/day_4.dart
@@ -0,0 +1,16 @@
+void main() {
+  //? to day class topic is if,else, else if,
+  print('day 4');
+  print('Module 2');
+
+  // final / run time constant
+  final String hi;
+  hi = 'Fokrul';
+  print(hi);
+
+//Const
+  const int hello = 10;
+  print(hello);
+
+  //
+}
