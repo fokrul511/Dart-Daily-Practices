2454e5ff0e56b94263bc25c9b85e028a07fe27c1
fokrul511 <islamfokrul2002@gmail.com>
2023-11-06 21:15:05 +0600

    update

--- lib/ostad_dart_lerning/day_4.dart
+++ lib/ostad_dart_lerning/day_4.dart
@@ -13,4 +13,20 @@ void main() {
   print(hello);
 
   //
+
+  //if........
+  // if (hello == 10) {
+  //   print('this is String');
+  // } else {
+  //   print('This is Not String');
+  // }
+
+  // day 2nd................
+
+  int num_1 = 50;
+  int num_2 = 100;
+
+  if (num_1 == num_2) {
+    print('two number is equals');
+  }
 }
