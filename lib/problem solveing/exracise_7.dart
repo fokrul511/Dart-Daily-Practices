0b23f70ba2a48131b5ca3d271cc9c08d9d45096f
fokrul511 <islamfokrul2002@gmail.com>
2023-11-07 21:57:55 +0600

    update

--- lib/problem solveing/exracise_7.dart
+++ lib/problem solveing/exracise_7.dart
@@ -1,10 +1,8 @@
 import 'dart:io';
 
-import 'package:ostad_dart_recode_video/function.dart';
-
 void main() {
   // print(minimum(10, 20, 30));
-}
+
 //! use function.....
 // int minimum(int a, int b, int c) {
 //   if (a < b && a < c) {
@@ -14,26 +12,27 @@ void main() {
 //   } else {
 //     return c;
 //   }
+/*
+    // //! stdout.write('Enter three numbers');
+    var one = int.parse(
+      stdin.readLineSync().toString(),
+    );
+    var two = int.parse(
+      stdin.readLineSync().toString(),
+    );
+    var three = int.parse(
+      stdin.readLineSync().toString(),
+    );
 
-  // //! stdout.write('Enter three numbers');
-  // var one = int.parse(
-  //   stdin.readLineSync().toString(),
-  // );
-  // var two = int.parse(
-  //   stdin.readLineSync().toString(),
-  // );
-  // var three = int.parse(
-  //   stdin.readLineSync().toString(),
-  // );
-
-  // if (one < two && one < three) {
-  //   print(one);
-  // } else if (two < one && two < three) {
-  //   print(two);
-  // } else {
-  //   print(three);
-  // }
-
+    if (one < two && one < three) {
+      print(one);
+    } else if (two < one && two < three) {
+      print(two);
+    } else {
+      print(three);
+    }
+  }
+*/
   //
 
 //   double a = 100.90;
@@ -53,3 +52,18 @@ void main() {
 
   // print(findMinimum(100, 20, 30));
 
+  //! final update..........
+  String num = stdin.readLineSync()!;
+  List<String> x = num.split(" ");
+  int a = int.parse(x[0]);
+  int b = int.parse(x[1]);
+  int c = int.parse(x[2]);
+
+  if (a <= b && a <= c) {
+    print(a);
+  } else if (b <= a && b <= c) {
+    print(b);
+  } else {
+    print(c);
+  }
+}
