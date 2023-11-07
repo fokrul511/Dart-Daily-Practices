7f75cd20c98f3b613184a7a29f1519000279e854
fokrul511 <islamfokrul2002@gmail.com>
2023-11-07 20:48:18 +0600

    update

---
+++ lib/problem solveing/exracise_7.dart
@@ -0,0 +1,55 @@
+import 'dart:io';
+
+import 'package:ostad_dart_recode_video/function.dart';
+
+void main() {
+  // print(minimum(10, 20, 30));
+}
+//! use function.....
+// int minimum(int a, int b, int c) {
+//   if (a < b && a < c) {
+//     return a;
+//   } else if (b < a && b < c) {
+//     return b;
+//   } else {
+//     return c;
+//   }
+
+  // //! stdout.write('Enter three numbers');
+  // var one = int.parse(
+  //   stdin.readLineSync().toString(),
+  // );
+  // var two = int.parse(
+  //   stdin.readLineSync().toString(),
+  // );
+  // var three = int.parse(
+  //   stdin.readLineSync().toString(),
+  // );
+
+  // if (one < two && one < three) {
+  //   print(one);
+  // } else if (two < one && two < three) {
+  //   print(two);
+  // } else {
+  //   print(three);
+  // }
+
+  //
+
+//   double a = 100.90;
+// // int.parse(a.toString()); // throws error
+//   var hel = int.parse(a.floor().toString());
+//   print(hel);
+
+  // int findMinimum(int a, int b, int c) {
+  //   if (a <= b && a <= c) {
+  //     return a;
+  //   } else if (b <= a && b <= c) {
+  //     return b;
+  //   } else {
+  //     return c;
+  //   }
+  // }
+
+  // print(findMinimum(100, 20, 30));
+
