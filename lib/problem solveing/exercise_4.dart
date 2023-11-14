f3978d86bf65bf521f3091bb7125f3f2c2e0915d
fokrul511 <islamfokrul2002@gmail.com>
2023-11-14 19:28:53 +0600

    day 4 update and print excuiteded file

--- lib/problem solveing/exercise_4.dart
+++ lib/problem solveing/exercise_4.dart
@@ -5,10 +5,10 @@ void main() {
   double netPrice;
   if (productWeight > 31) {
     price = productWeight * 10.5;
-  }else{
-    price = productWeight*10.56;
+  } else {
+    price = productWeight * 10.56;
   }
-  vat = vat/100;
-  netPrice = (price*vat)+price;
-  print(price);
+  vat = vat / 100;
+  netPrice = (price * vat) + price;
+  print(netPrice);
 }
