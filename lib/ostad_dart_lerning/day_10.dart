185ff2136a3e042e19d5675557a047365bc46ead
fokrul511 <islamfokrul2002@gmail.com>
2023-11-15 13:56:04 +0600

    day 10 oop advanch

--- lib/ostad_dart_lerning/day_10.dart
+++ lib/ostad_dart_lerning/day_10.dart
@@ -1,3 +1,19 @@
 void main() {
   print('day 10');
+  //?
+  //advance oop concept
+  Food kola = Food(foodName: 'Kola', price: 20);
+  print('food name:${kola.foodName} price is:${kola.price}');
+}
+
+class Food {
+  String? foodName;
+  int? price;
+  String? description;
+  int? dateTime;
+  Food(
+      {required this.foodName,
+      required this.price,
+      this.description,
+      this.dateTime});
 }
