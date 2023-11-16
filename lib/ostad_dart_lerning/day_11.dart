bd0f4898facc083e2f8f4e19ff1d06814be5c039
fokrul511 <islamfokrul2002@gmail.com>
2023-11-16 18:37:00 +0600

    day 11 update

--- lib/ostad_dart_lerning/day_11.dart
+++ lib/ostad_dart_lerning/day_11.dart
@@ -2,3 +2,22 @@ void main() {
   print('day 11 ');
   // today practice oop advance day 11 topic
 }
+// today topic is construer oop class
+
+class Day11 {
+  late String name;
+  late int roll;
+  late int reg;
+  late int age;
+  Day11(
+      {required this.name,
+      required this.roll,
+      required this.reg,
+      required this.age});
+}
+
+//! create data list.........
+List day11 = [
+  Day11(name: 'Fokrul', roll: 476639, reg: 1818191900, age: 22),
+  Day11(name: 'Samiya', roll: 575429, reg: 4778893890, age: 18),
+];
