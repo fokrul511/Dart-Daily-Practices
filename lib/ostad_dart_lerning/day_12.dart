d9159dc9d8e3652397888c25c6949c3c2434c0f8
fokrul511 <islamfokrul2002@gmail.com>
2023-11-17 19:32:46 +0600

    update 12

--- lib/ostad_dart_lerning/day_12.dart
+++ lib/ostad_dart_lerning/day_12.dart
@@ -3,15 +3,11 @@ void main() {
   // today introduce Inheritance
   print('day 12');
   Student student1 = Student('sdsd', 54664, 'dfdsfdsfsdf');
-  print(student1);
-
-  //
+  print(student1.hello(
+      'Fokrul')); // instance another method under class call main function and print
 }
 
 class Student {
-  // String name = '';
-  // int roll = 000000;
-  // String address = '';
   String? name;
   String? address;
 
@@ -20,7 +16,7 @@ class Student {
   Student(this.name, this.roll, this.address);
 
   //? class everything is method.....
-  void hello(String name) {
-    print('$name this is name');
+  hello(String name) {
+    print(' this name is $name');
   }
 }
