0717d52907ee9e94dda09d7dd86a1547474d38c9
fokrul511 <islamfokrul2002@gmail.com>
2023-11-18 08:35:37 +0600

    list file update complex think

--- lib/List/list.dart
+++ lib/List/list.dart
@@ -1,30 +1,21 @@
 void main() {
-  List<Object> complexList = [
-    12,
-    232,
-    2323,
-    23,
-    'hello',
-    true,
-    false,
-    20.9,
-    A()
-  ];
-  print(complexList.runtimeType);
-  print(complexList);
+  List<int> complexList = [];
+  List<int> complexLihavevalue = [12, 23, 43, 54];
+  //
+  List<int> complexListFixvalue = List.filled(10, 0, growable: true);
+  complexListFixvalue[0] = 20;
+  //
+  List<int> complexListEmpty = List.empty();
+  //
+  List<int> complexListGen =
+      List.generate(complexLihavevalue.length, (index) => index);
+  // print(complexListFixvalue);
 
-  var integerValue = complexList[0] as int;
-  var doubbleValue = complexList[1] as double;
-  var stringValue = complexList[2] as String;
-  var booleanValue = complexList[3] as bool;
-  var nullValue = complexList[4] as Null;
+  print(complexListGen);
+  // print(complexLihavevalue);
+  // print(complexList);
 
   //
-
-  List<int> a = [1, 2, 4, 5];
-  List<int?> b = [1, 2, 4, 5, null];
-  List<int>? c = [1, 2, 4, 5];
-  List<int?>? d = [1, 2, 4, 5, 7, null];
 }
 
 class A {}
