d01f367d66122549bb76504e11c43ff87e1eff3d
fokrul511 <islamfokrul2002@gmail.com>
2023-11-21 21:25:24 +0600

    update day 13

--- lib/List/list.dart
+++ lib/List/list.dart
@@ -1,11 +1,11 @@
 void main() {
-  List<int> complexList = [];
+  // List<int> complexList = [];
   List<int> complexLihavevalue = [12, 23, 43, 54];
   //
   List<int> complexListFixvalue = List.filled(10, 0, growable: true);
   complexListFixvalue[0] = 20;
   //
-  List<int> complexListEmpty = List.empty();
+  // List<int> complexListEmpty = List.empty();
   //
   List<int> complexListGen =
       List.generate(complexLihavevalue.length, (index) => index);
