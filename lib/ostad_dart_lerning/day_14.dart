b0a8b927e68ba26479e256329632e9ae1f6180ea
fokrul511 <islamfokrul2002@gmail.com>
2023-11-24 12:10:28 +0600

    abstract use class

--- lib/ostad_dart_lerning/day_14.dart
+++ lib/ostad_dart_lerning/day_14.dart
@@ -1,1 +1,18 @@
-void main() {}
+void main() {
+  //var details = Father();// when we use class under abstract ..this time can't call this class any function...
+  var bacc = Son(); // this time we can call Father() ,class
+  bacc.Land(); // !this is very esy....
+}
+// ?abstract use class
+
+abstract class Father {
+  Land() {
+    print('1000 taka ');
+  }
+}
+
+class Son extends Father {
+  littleBaby() {
+    print('my son have 1000 taka school fess');
+  }
+}
