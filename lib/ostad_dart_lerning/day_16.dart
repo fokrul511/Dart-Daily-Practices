9c4155371ca17f45fa81889446de859e695a5813
fokrul511 <islamfokrul2002@gmail.com>
2023-11-26 21:55:34 +0600

    update day 16

--- lib/ostad_dart_lerning/day_16.dart
+++ lib/ostad_dart_lerning/day_16.dart
@@ -1,1 +1,12 @@
-void main() {}
+void main() {
+  NewNumber.result(2, 5);
+}
+
+class NewNumber {
+  // int? number1;
+  // int? number2;
+  static void result(int num1, int num2) {
+    var result = num1 + num2;
+    print("total number is: $result");
+  }
+}
