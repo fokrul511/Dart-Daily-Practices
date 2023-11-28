8771c09eda7c85e977e130f1f134e658821b1edd
fokrul511 <islamfokrul2002@gmail.com>
2023-11-28 17:58:03 +0600

    update day 17

--- lib/ostad_dart_lerning/day_17.dart
+++ lib/ostad_dart_lerning/day_17.dart
@@ -1,4 +1,15 @@
 void main() {
-  print('day 17');
+  // print('day 17');
   //
+  Mr h = Mr(30);
+  h.p();
+}
+//inheritance...........
+
+class Mr {
+  int? m;
+  Mr(this.m);
+  void p() {
+    print(this.m);
+  }
 }
