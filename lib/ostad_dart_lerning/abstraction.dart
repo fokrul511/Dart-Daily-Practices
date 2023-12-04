f1c9d05c6a9ad033feee02a11eb3fbdf3289a13f
fokrul511 <islamfokrul2002@gmail.com>
2023-12-04 08:08:08 +0600

    update

--- lib/ostad_dart_lerning/abstraction.dart
+++ lib/ostad_dart_lerning/abstraction.dart
@@ -22,7 +22,108 @@ class Car {
   void _gpsConnect() {}
 }
 
+abstract class DebugTest {
+  void debug();
+} //temp class
+
 void main() {
   Car bmw = Car('bmw', '8 saris');
   bmw.startTheCar();
+}
+
+abstract class Mobile {
+  //? all of interface.........................abstract class
+  void call();
+  void onTouch();
+  void turnoff();
+}
+
+class Iphone extends Mobile {
+  //? implements..................................
+  @override
+  void call() {
+    // TODO: implement call
+  }
+
+  @override
+  void onTouch() {
+    // TODO: implement onTouch
+  }
+
+  @override
+  void turnoff() {
+    // TODO: implement turnoff
+  }
+}
+
+//22 mint
+class Samsung implements Mobile, DebugTest {
+  @override
+  void call() {
+    // TODO: implement call
+  }
+
+  @override
+  void onTouch() {
+    // TODO: implement onTouch
+  }
+
+  @override
+  void turnoff() {
+    // TODO: implement turnoff
+    _xyz();
+    print('hisdsjd');
+  }
+
+  void _xyz() {
+    print('djkdfxdjf');
+  }
+
+  @override
+  void debug() {
+    // TODO: implement debug
+  }
+}
+
+//*dart not support multiple inheritance....only support single inheritance
+//*but multiple support implication keyword....
+
+//
+
+class XYZ implements Car {
+  @override
+  late String model;
+
+  @override
+  late String name;
+
+  @override
+  void _connectController() {
+    // TODO: implement _connectController
+  }
+
+  @override
+  void _connectWithLight() {
+    // TODO: implement _connectWithLight
+  }
+
+  @override
+  void _gpsConnect() {
+    // TODO: implement _gpsConnect
+  }
+
+  @override
+  void _startDiMeter() {
+    // TODO: implement _startDiMeter
+  }
+
+  @override
+  void _startEngine() {
+    // TODO: implement _startEngine
+  }
+
+  @override
+  void startTheCar() {
+    // TODO: implement startTheCar
+  }
 }
---
+++ lib/ostad_dart_lerning/null_sefty.dart
@@ -0,0 +1,18 @@
+void main() {
+  int? a;
+  int? b;
+  late String name;
+  name = 'fokrul';
+  print(name);
+  // if (b == null) {
+  //   print(1);
+  // } else {
+  //   print(b);
+  // }
+  // ??-if-null
+  print(a ?? b);
+  // b = 50;
+//force unwrap
+  // print(b!);
+}
+//!  late
