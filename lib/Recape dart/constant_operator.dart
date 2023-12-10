11f1bafcc947cd90956c83e791da918e1e8caff8
fokrul511 <islamfokrul2002@gmail.com>
2023-12-10 09:30:36 +0600

    update

---
+++ lib/Recape dart/constant_operator.dart
@@ -0,0 +1,13 @@
+String name = 'fokrul';
+int age = 20;
+double gpa = 3.31;
+bool condition = true;
+var anyValu = false; // if want any type valu store var variable....one time
+//List...........................
+List<String> studentList = ['MPi', 'syl', 'gmm', 'dmm'];
+
+//constant data
+void main() {
+  studentList.add('hi');
+  print(studentList.elementAt(2));
+}
